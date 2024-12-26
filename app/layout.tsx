7380023b861d60c6ca2b57e0ebba48161d417628
Peter Ahumuza <hmz2peter@gmail.com>
2024-12-26 22:44:11 +0200

    changed font to poppins

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,15 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 import Link from "next/link";
 import { AuthProvider } from "@/context/auth";
 import AuthButtons from "@/components/AuthButtons";
 import { HomeIcon } from "lucide-react";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const poppins = Poppins({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+  weight: ["100", "200", "300", "400","500","600","700","800","900"]
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -29,7 +24,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${poppins.className} antialiased`}
       >
         <AuthProvider>
           <nav className="bg-sky-950 text-white p-5 h-24 flex items-center justify-between">
