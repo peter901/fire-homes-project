45cea4bc7d47e89bb39841894529b0b8e21daea7
Peter Ahumuza <hmz2peter@gmail.com>
2024-12-26 09:09:54 +0200

    added signin and signup links

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +28,19 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <nav className="bg-sky-950 text-white p-5 h-24 flex items-center justify-between">
+          <Link href="/">
+            Fire homes
+          </Link>
+          <ul>
+            <li>
+              <Link href="/login">Signin</Link>
+            </li>
+            <li>
+              <Link href="/signup">Signup</Link>
+            </li>
+          </ul>
+        </nav>
         {children}
       </body>
     </html>
