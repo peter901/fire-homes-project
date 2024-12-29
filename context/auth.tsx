cf291ba77b55e43b1e500af71dcedf846529e80e
Peter Ahumuza <hmz2peter@gmail.com>
2024-12-29 13:57:00 +0300

    setup context/auth to store token and refreshToken

--- context/auth.tsx
+++ context/auth.tsx
@@ -1,8 +1,9 @@
 "use client"
 
 import { auth } from "@/firebase/client";
-import { User,GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { User,GoogleAuthProvider, signInWithPopup, ParsedToken } from "firebase/auth";
 import { createContext, useContext, useEffect, useState } from "react";
+import { setToken } from "./actions";
 
 type AuthContextType = {
     currentUser: User | null;
@@ -16,10 +17,29 @@ export const AuthProvider = ({children}: {
     children : React.ReactNode
 }) =>{
     const [currentUser, setCurrentUser] = useState<User | null>(null);
+    const [customClaims, setCustomClaims] = useState<ParsedToken | null>()
 
     useEffect(()=>{
-        const unsubscribe = auth.onAuthStateChanged((user)=>{
+        const unsubscribe = auth.onAuthStateChanged(async (user)=>{
             setCurrentUser(user ?? null)
+
+            if(user){
+                const tokenResult = await user.getIdTokenResult();
+                const token = tokenResult.token;
+                const refreshToken = user.refreshToken;
+
+                const claims = tokenResult.claims;
+
+                setCustomClaims(claims ?? null)
+
+                if(token && refreshToken){
+                    await setToken({
+                        token, 
+                        refreshToken
+                    });
+                }
+
+            }
         })
 
         return () => unsubscribe()
