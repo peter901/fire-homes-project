5a4bf496e80078b88dfc576062466559c7e22370
Peter Ahumuza <hmz2peter@gmail.com>
2025-01-01 11:16:33 +0200

    added button to the admin dashboard

--- app/admin-dashboard/page.tsx
+++ app/admin-dashboard/page.tsx
@@ -1,4 +1,7 @@
 import { Breadcrumbs } from "@/components/ui/breadcrumb";
+import { Button } from "@/components/ui/button";
+import { PlusCircle } from "lucide-react";
+import Link from "next/link";
 
 export default function AdminDashboard(){
     return (
@@ -8,6 +11,12 @@ export default function AdminDashboard(){
             }/>
 
             <h1 className="text-4xl font-bold mt-6">Admin Dashboard</h1>
+
+            <Button asChild className="inline-flex pl-2 gap-2 mt-4">
+                <Link href="/admin-dashboard/new">
+                   <PlusCircle/> New Property
+                </Link>
+            </Button>
         </div>
     )
 }
