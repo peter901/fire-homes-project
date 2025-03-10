497fc31156368ec5e701a235e3bf7b1c3bebe9c4
Peter Ahumuza <hmz2peter@gmail.com>
2025-03-10 13:31:11 +0100

    disable form when submitting

--- components/ui/PropertyForm.tsx
+++ components/ui/PropertyForm.tsx
@@ -50,7 +50,10 @@ export default function PropertyForm({
     <Form {...form}>
       <form onSubmit={form.handleSubmit(handleSubmit)}>
         <div className="grid grid-cols-2 gap-4">
-          <fieldset className="flex flex-col gap-2">
+          <fieldset
+            className="flex flex-col gap-2"
+            disabled={form.formState.isSubmitting}
+          >
             <FormField
               control={form.control}
               name="status"
@@ -130,7 +133,10 @@ export default function PropertyForm({
               )}
             />
           </fieldset>
-          <fieldset className="flex flex-col gap-2">
+          <fieldset
+            className="flex flex-col gap-2"
+            disabled={form.formState.isSubmitting}
+          >
             <FormField
               control={form.control}
               name="price"
@@ -188,6 +194,7 @@ export default function PropertyForm({
         <Button
           type="submit"
           className="max-w-md mx-auto mt-2 w-full flex gap-2"
+          disabled={form.formState.isSubmitting}
         >
           {submitButtonLabel}
         </Button>
