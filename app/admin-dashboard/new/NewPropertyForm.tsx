8734c06d5db2250c190a58ae11635336f33a637d
Peter Ahumuza <hmz2peter@gmail.com>
2025-03-10 03:50:14 +0100

    added property form

--- app/admin-dashboard/new/NewPropertyForm.tsx
+++ app/admin-dashboard/new/NewPropertyForm.tsx
@@ -1,12 +1,14 @@
 "use client";
-import PropertyForm from "@/components/ui/propertyForm";
+import PropertyForm from "@/components/ui/PropertyForm";
+import type { infer as zodInfer } from "zod";
+import type { propertyDataSchema } from "@/validation/propertySchema";
 
 
 export default function NewPropertyForm() {
-    
+    const handleSubmit = async (data: zodInfer<typeof propertyDataSchema>) =>{}
   return (
     <div>
-      <PropertyForm />
+      <PropertyForm handleSubmit={handleSubmit} />
     </div>
   );
 }
