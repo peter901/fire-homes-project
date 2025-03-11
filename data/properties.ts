f2cc9d8f4e96a09e0760dccca8f43aaa8f6b4ba3
Peter Ahumuza <hmz2peter@gmail.com>
2025-03-11 03:27:07 +0100

    add property type to properties

--- data/properties.ts
+++ data/properties.ts
@@ -1,4 +1,5 @@
 import { firestore } from "@/firebase/server";
+import type { Property } from "@/types/property";
 import type { PropertyStatus } from "@/types/propertyStatus";
 import "server-only";
 
@@ -48,7 +49,7 @@ export const getProperties = async (options?: GetPropertiesOptions) => {
   const properties = propertiesSnapshot.docs.map((doc) => ({
     id: doc.id,
     ...doc.data(),
-  }));
+  } as Property));
 
   return { data: properties };
 };
