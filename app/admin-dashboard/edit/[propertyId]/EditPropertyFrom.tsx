3e0b3d8bb9032070ea6e3596d6044a5fb4eb11e9
Peter Ahumuza <hmz2peter@gmail.com>
2025-03-11 19:02:17 +0100

    modify edit propery page

--- app/admin-dashboard/edit/[propertyId]/EditPropertyFrom.tsx
+++ app/admin-dashboard/edit/[propertyId]/EditPropertyFrom.tsx
@@ -1,15 +1,41 @@
 "use client";
 
+import PropertyForm from "@/components/ui/PropertyForm";
 import type { Property } from "@/types/property";
+import type { propertyDataSchema } from "@/validation/propertySchema";
+import type { infer as zodInfer } from "zod";
 
 export default function EditPropertyFrom({
   id,
   address1,
   address2,
+  city,
   bedrooms,
   bathrooms,
+  description,
+  postcode,
   price,
   status,
 }: Property) {
-  return <div>EditPropertyFrom</div>;
+  const handleSubmit = async (data: zodInfer<typeof propertyDataSchema>) => {};
+
+  return (
+    <div>
+      <PropertyForm
+        handleSubmit={handleSubmit}
+        submitButtonLabel="Edit Property"
+        defaultValues={{
+          address1,
+          address2,
+          city,
+          bedrooms,
+          bathrooms,
+          description,
+          postcode,
+          price,
+          status,
+        }}
+      />
+    </div>
+  );
 }
