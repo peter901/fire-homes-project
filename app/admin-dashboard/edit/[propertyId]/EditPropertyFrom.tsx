db218fa258f45c17584377f71d1e308bd99466df
Peter Ahumuza <hmz2peter@gmail.com>
2025-03-11 18:50:07 +0100

    add edit property form

---
+++ app/admin-dashboard/edit/[propertyId]/EditPropertyFrom.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import type { Property } from "@/types/property";
+
+export default function EditPropertyFrom({
+  id,
+  address1,
+  address2,
+  bedrooms,
+  bathrooms,
+  price,
+  status,
+}: Property) {
+  return <div>EditPropertyFrom</div>;
+}
