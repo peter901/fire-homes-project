103d67e9289551e6bbf38ee96842c6d1968043d1
Peter Ahumuza <hmz2peter@gmail.com>
2025-03-12 21:40:26 +0100

    add placeholder when dragging items

--- components/ui/MultiImageUploader.tsx
+++ components/ui/MultiImageUploader.tsx
@@ -119,6 +119,7 @@ export default function MultiImageUploader({
                   )}
                 </Draggable>
               ))}
+              {provided.placeholder}
             </div>
           )}
         </Droppable>
