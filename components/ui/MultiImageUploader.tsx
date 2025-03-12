341e396c4506c7077a9e97ae710dacf5f0272953
Peter Ahumuza <hmz2peter@gmail.com>
2025-03-12 18:00:49 +0100

    implement handleInputChange and format

--- components/ui/MultiImageUploader.tsx
+++ components/ui/MultiImageUploader.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useRef } from "react";
+import React, { useRef } from "react";
 import { Button } from "./button";
 
 export type ImageUpload = {
@@ -16,12 +16,27 @@ export default function MultiImageUploader({
   images?: ImageUpload[];
   onImagesChange: (images: ImageUpload[]) => void;
 }) {
-    const uploadInputRef = useRef<HTMLInputElement | null>(null);
+  const uploadInputRef = useRef<HTMLInputElement | null>(null);
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const files = Array.from(e.target.files || []);
+
+    console.log(files);
+  };
 
   return (
     <div className="w-full max-w-3xl mx-auto p-4 ">
-        <input className="hidden" ref={uploadInputRef} type="file" multiple accept="image/*" />
-      <Button type="button" onClick={() => uploadInputRef?.current?.click()}>Upload images</Button>
+      <input
+        className="hidden"
+        ref={uploadInputRef}
+        onChange={handleInputChange}
+        type="file"
+        multiple
+        accept="image/*"
+      />
+      <Button type="button" onClick={() => uploadInputRef?.current?.click()}>
+        Upload images
+      </Button>
     </div>
   );
 }
