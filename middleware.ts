48ae24f5874c7ab1b92dbe51bdba739a499d65fb
Peter Ahumuza <hmz2peter@gmail.com>
2025-03-12 05:04:54 +0100

    protect login route && format code

--- middleware.ts
+++ middleware.ts
@@ -2,29 +2,31 @@ import { cookies } from "next/headers";
 import { type NextRequest, NextResponse } from "next/server";
 import { decodeJwt } from "jose";
 
-export async function middleware(request : NextRequest){
-    if(request.method ==="POST"){
-        return NextResponse.next();
-    }
-
-    const cookieStore = await cookies();
-    const token = cookieStore.get("firebaseAuthToken")?.value;
-
-    if(!token){
-        return NextResponse.redirect(new URL("/", request.url))
-    }
-
-    const decodedToken = decodeJwt(token);
-
-    if(!decodedToken.admin){
-        return NextResponse.redirect(new URL("/", request.url))
-    }
-
+export async function middleware(request: NextRequest) {
+  if (request.method === "POST") {
     return NextResponse.next();
+  }
+
+  const cookieStore = await cookies();
+  const token = cookieStore.get("firebaseAuthToken")?.value;
+
+  if(token && request.nextUrl.pathname.startsWith("/login")){
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+
+  if (!token) {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+
+  const decodedToken = decodeJwt(token);
+
+  if (!decodedToken.admin) {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+
+  return NextResponse.next();
 }
 
 export const config = {
-    matcher : [
-        "/admin-dashboard","/admin-dashboard/:path*",
-    ]
-}
+  matcher: ["/admin-dashboard", "/admin-dashboard/:path*","/login"],
+};
