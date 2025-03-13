ea16d4c9ae09c53308b7afb2580426fad080a518
Peter Ahumuza <hmz2peter@gmail.com>
2025-03-13 13:27:57 +0100

    display property page

--- app/property/[propertyId]/page.tsx
+++ app/property/[propertyId]/page.tsx
@@ -1,6 +1,8 @@
 import { Button } from "@/components/ui/button";
+import PropertyStatusBadge from "@/components/ui/property-badge";
 import { getPropertyById } from "@/data/properties";
-import { ArrowLeftIcon } from "lucide-react";
+import { ArrowLeftIcon, BathIcon, BedIcon } from "lucide-react";
+import numeral from "numeral";
 import ReactMarkDown from "react-markdown";
 
 export default async function PropertyPage({
@@ -10,8 +12,15 @@ export default async function PropertyPage({
 }) {
   const property = await getPropertyById((await params).propertyId);
 
+  const addressLines = [
+    property.address1,
+    property.address2,
+    property.city,
+    property.postcode,
+  ].filter((addressLines) => addressLines);
+
   return (
-    <div className="grid grid-cols-[1fr_400px]">
+    <div className="grid grid-cols-[1fr_500px]">
       <div>
         <div className="property-descrition max-w-screen-md mx-auto py-10 px-4">
           <Button>
@@ -20,7 +29,30 @@ export default async function PropertyPage({
           <ReactMarkDown>{property.description}</ReactMarkDown>
         </div>
       </div>
-      <div className="bg-sky-200 h-screen sticky top-0">property details</div>
+      <div className="bg-sky-200 h-screen p-10 sticky top-0 grid ">
+        <div className="flex flex-col gap-10 w-full">
+          <PropertyStatusBadge status={property.status} className="mr-auto text-base" />
+          <h1 className="text-4xl font-semibold">
+            {addressLines.map((addressLine, i)=>(
+              <div key={addressLine}>
+                {addressLine}
+                {i < addressLines.length -1 && ","}
+              </div>
+            ))}
+          </h1>
+          <h2 className="text-3xl font-light">
+            Ugx {numeral(property.price).format("0,0")}
+          </h2>
+          <div className="flex gap-10">
+            <div className="flex gap-2">
+              <BedIcon /> {property.bedrooms} Bedrooms
+            </div>
+            <div className="flex gap-2">
+              <BathIcon /> {property.bathrooms} Bathrooms
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
