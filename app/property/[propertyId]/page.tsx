6073af1c472ba9d7d5554b8ade4c8b93276931b2
Peter Ahumuza <hmz2peter@gmail.com>
2025-03-13 13:05:41 +0100

    Render property description

--- app/property/[propertyId]/page.tsx
+++ app/property/[propertyId]/page.tsx
@@ -1,4 +1,6 @@
+import { Button } from "@/components/ui/button";
 import { getPropertyById } from "@/data/properties";
+import { ArrowLeftIcon } from "lucide-react";
 import ReactMarkDown from "react-markdown";
 
 export default async function PropertyPage({
@@ -11,7 +13,12 @@ export default async function PropertyPage({
   return (
     <div className="grid grid-cols-[1fr_400px]">
       <div>
-        <ReactMarkDown>{property.description}</ReactMarkDown>
+        <div className="property-descrition max-w-screen-md mx-auto py-10 px-4">
+          <Button>
+            <ArrowLeftIcon /> Back
+          </Button>
+          <ReactMarkDown>{property.description}</ReactMarkDown>
+        </div>
       </div>
       <div className="bg-sky-200 h-screen sticky">property details</div>
     </div>
