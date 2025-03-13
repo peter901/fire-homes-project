3258fac29563c3f0206c81527a82b59777f79b54
Peter Ahumuza <hmz2peter@gmail.com>
2025-03-13 12:49:20 +0100

    render property details in react markdown

--- app/property/[propertyId]/page.tsx
+++ app/property/[propertyId]/page.tsx
@@ -1,6 +1,19 @@
+import { getPropertyById } from "@/data/properties";
+import ReactMarkDown from "react-markdown";
 
-export default function PropertyPage() {
+export default async function PropertyPage({
+  params,
+}: {
+  params: Promise<{ propertyId: string }>;
+}) {
+  const property = await getPropertyById((await params).propertyId);
+
   return (
-    <div>PropertyPage</div>
-  )
+    <div className="grid grid-cols-[1fr_400px]">
+      <div>
+        <ReactMarkDown>{property.description}</ReactMarkDown>
+      </div>
+      <div className="bg-sky-200 h-screen sticky">property details</div>
+    </div>
+  );
 }
