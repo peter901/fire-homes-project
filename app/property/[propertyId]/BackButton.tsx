b8809d8d48c95e7196e58f02053ed4b8f558fc1b
Peter Ahumuza <hmz2peter@gmail.com>
2025-03-13 22:00:40 +0100

    add router to back button

--- app/property/[propertyId]/BackButton.tsx
+++ app/property/[propertyId]/BackButton.tsx
@@ -2,10 +2,12 @@
 
 import { Button } from "@/components/ui/button";
 import { ArrowLeftIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 export default function BackButton() {
+    const router = useRouter()
   return (
-    <Button>
+    <Button onClick={() => router.back()}>
       <ArrowLeftIcon /> Back
     </Button>
   );
