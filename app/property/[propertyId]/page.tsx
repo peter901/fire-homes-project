5c946e6e836b642e43858ddd5731fbe50a8d456c
Peter Ahumuza <hmz2peter@gmail.com>
2025-03-13 21:52:50 +0100

    added carousel for image display

--- app/property/[propertyId]/page.tsx
+++ app/property/[propertyId]/page.tsx
@@ -1,7 +1,15 @@
 import { Button } from "@/components/ui/button";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 import PropertyStatusBadge from "@/components/ui/property-badge";
 import { getPropertyById } from "@/data/properties";
 import { ArrowLeftIcon, BathIcon, BedIcon } from "lucide-react";
+import Image from "next/image";
 import numeral from "numeral";
 import ReactMarkDown from "react-markdown";
 
@@ -22,6 +30,33 @@ export default async function PropertyPage({
   return (
     <div className="grid grid-cols-[1fr_500px]">
       <div>
+        {property.images && (
+          <Carousel className="w-full">
+            <CarouselContent>
+              {property.images.map((image) => (
+                <CarouselItem key={image}>
+                  <div className="relative h-[80vh] m-h-80">
+                    <Image
+                      src={`https://firebasestorage.googleapis.com/v0/b/fire-homes-project-eeac6.firebasestorage.app/o/${encodeURIComponent(
+                        image
+                      )}?alt=media`}
+                      alt=""
+                      fill
+                      className="object-cover"
+                    />
+                  </div>
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+            {property.images.length > 1 && (
+              <>
+                <CarouselPrevious className="translate-x-24" />
+                <CarouselNext className="-translate-x-24" />
+              </>
+            )}
+          </Carousel>
+        )}
+
         <div className="property-descrition max-w-screen-md mx-auto py-10 px-4">
           <Button>
             <ArrowLeftIcon /> Back
@@ -31,12 +66,15 @@ export default async function PropertyPage({
       </div>
       <div className="bg-sky-200 h-screen p-10 sticky top-0 grid ">
         <div className="flex flex-col gap-10 w-full">
-          <PropertyStatusBadge status={property.status} className="mr-auto text-base" />
+          <PropertyStatusBadge
+            status={property.status}
+            className="mr-auto text-base"
+          />
           <h1 className="text-4xl font-semibold">
-            {addressLines.map((addressLine, i)=>(
+            {addressLines.map((addressLine, i) => (
               <div key={addressLine}>
                 {addressLine}
-                {i < addressLines.length -1 && ","}
+                {i < addressLines.length - 1 && ","}
               </div>
             ))}
           </h1>
