e58a31b5c5135a452a4b86d57f8a389e3b8d671e
Peter Ahumuza <hmz2peter@gmail.com>
2025-03-14 15:28:26 +0100

    handle requests to signup depending on whether or not token is set

--- middleware.ts
+++ middleware.ts
@@ -10,6 +10,14 @@ export async function middleware(request: NextRequest) {
   const cookieStore = await cookies();
   const token = cookieStore.get("firebaseAuthToken")?.value;
 
+  if (!token && request.nextUrl.pathname.startsWith("/signup")) {
+    return NextResponse.next();
+  }
+
+  if (token && request.nextUrl.pathname.startsWith("/signup")) {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+
   if (!token && request.nextUrl.pathname.startsWith("/login")) {
     return NextResponse.next();
   }
