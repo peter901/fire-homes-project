4c245a3d957d3e801a59069e491d0887c59f47db
Peter Ahumuza <hmz2peter@gmail.com>
2025-03-14 08:20:07 +0100

    add all search fields to the filter

--- app/property-search/FiltersForm.tsx
+++ app/property-search/FiltersForm.tsx
@@ -1,5 +1,87 @@
 "use client";
 
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import z from "zod";
+import type { infer as zodInfer } from "zod";
+
+const formSchema = z.object({
+  minPrice: z.string().optional(),
+  maxPrice: z.string().optional(),
+  minBedrooms: z.string().optional(),
+});
+
 export default function FiltersForm() {
-  return <div>FiltersForm</div>;
+  const form = useForm<zodInfer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      maxPrice: "",
+      minPrice: "",
+      minBedrooms: "",
+    },
+  });
+
+  const handleSubmit = async (data: zodInfer<typeof formSchema>) => {
+    console.log(data);
+  };
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(handleSubmit)}
+        className="grid grid-cols-4 gap-2"
+      >
+        <FormField
+          control={form.control}
+          name="minPrice"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input
+                  {...field}
+                  placeholder="Min Price"
+                  type="number"
+                  min={0}
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="maxPrice"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input
+                  {...field}
+                  placeholder="Max Price"
+                  type="number"
+                  min={0}
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="minBedrooms"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input
+                  {...field}
+                  placeholder="Min Bedrooms"
+                  type="number"
+                  min={0}
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+      </form>
+    </Form>
+  );
 }
