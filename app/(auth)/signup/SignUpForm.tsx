b7d600963f5f67d9146190aba94f51188ecef2e0
Peter Ahumuza <hmz2peter@gmail.com>
2025-03-14 16:58:52 +0100

    add form schema

--- app/(auth)/signup/SignUpForm.tsx
+++ app/(auth)/signup/SignUpForm.tsx
@@ -1,8 +1,34 @@
 "use client";
 
+import z from "zod";
+
+const formSchema = z
+  .object({
+    email: z.string().email(),
+    name: z.string().min(2, "Name must be longer than 2 charaters"),
+    password: z.string(),
+    confirmPassowrd: z.string().refine(
+      (value) => {
+        const regex =
+          /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/;
+        return regex.test(value);
+      },
+      {
+        message:
+          "Password must contain atleast 6 characters, an uppercase letter, lower case letter, a number and a special character",
+      }
+    ),
+  })
+  .superRefine((data, ctx) => {
+    if (data.password !== data.confirmPassowrd) {
+      ctx.addIssue({
+        message: "Passwords do not match",
+        path: ["passwordConfirm"],
+        code: "custom",
+      });
+    }
+  });
 
 export default function SignUpForm() {
-  return (
-    <div>SignUpForm</div>
-  )
+  return <div>SignUpForm</div>;
 }
