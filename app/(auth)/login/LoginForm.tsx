6229f9056c771c453b2ee962f44156f18bd6b0ca
Peter Ahumuza <hmz2peter@gmail.com>
2025-03-15 06:10:37 +0100

    added login form component

---
+++ app/(auth)/login/LoginForm.tsx
@@ -0,0 +1,93 @@
+"use client";
+
+import ContinueWithGoogleButton from "@/components/ContinueWithGoogleButton";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import type { infer as zodInfer } from "zod";
+import { loginUserSchema } from "@/validation/loginSchema";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
+import { loginUser } from "./actions";
+
+export default function LoginForm() {
+  const router = useRouter();
+  const form = useForm<zodInfer<typeof loginUserSchema>>({
+    resolver: zodResolver(loginUserSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const handleSubmit = async (data: zodInfer<typeof loginUserSchema>) => {
+    const response = await loginUser(data);
+
+    if (response?.error) {
+      toast("Error!!!", {
+        description: response.message,
+      });
+      return;
+    }
+
+    toast("Success", {
+      description: "Sign up successfully",
+    });
+
+    router.push("/login");
+  };
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(handleSubmit)}
+        className="flex flex-col gap-4"
+      >
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => {
+            return (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input {...field} placeholder="Email" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            );
+          }}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => {
+            return (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input {...field} placeholder="Password" type="password" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            );
+          }}
+        />
+        <Button type="submit">Login</Button>
+        <div className="text-center">or</div>
+      </form>
+      <div className="mt-4">
+        <ContinueWithGoogleButton />
+      </div>
+    </Form>
+  );
+}
