5130afbcf52ebda8a58d8d45b522ddd2b85878ce
Peter Ahumuza <hmz2peter@gmail.com>
2025-03-16 09:15:27 +0100

    refresh page when heart icon is clicked

--- app/property-search/FavouriteToggleButton.tsx
+++ app/property-search/FavouriteToggleButton.tsx
@@ -2,6 +2,7 @@
 import { HeartIcon } from "lucide-react";
 import { addFavourite } from "./actions";
 import { useAuth } from "@/context/auth";
+import { useRouter } from "next/navigation";
 
 export default function FavouriteToggleButton({
   propertyId,
@@ -11,19 +12,21 @@ export default function FavouriteToggleButton({
   isFavourite: boolean;
 }) {
   const auth = useAuth();
+  const router = useRouter();
   return (
     <button
       type="button"
-      className="absolute top-0 right-0 z-10 p-2"
+      className="absolute top-0 right-0 z-10 p-2 bg-white rounded-bl-lg"
       onClick={async () => {
         const tokenResult = await auth?.currentUser?.getIdTokenResult();
         if (!tokenResult) {
           return;
         }
         await addFavourite(propertyId, tokenResult.token);
+        router.refresh();
       }}
     >
-      <HeartIcon fill={isFavourite ? "blue" : "white"} />
+      <HeartIcon className="text-black" fill={isFavourite ? "blue" : "white"} />
     </button>
   );
 }
