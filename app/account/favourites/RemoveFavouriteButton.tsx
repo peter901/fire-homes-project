00b166a6bb7e0ef674dc0c549dbfa3090ffbd20a
Peter Ahumuza <hmz2peter@gmail.com>
2025-03-16 22:40:04 +0100

    remove from favourites button

---
+++ app/account/favourites/RemoveFavouriteButton.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { removeFavourite } from "@/app/property-search/actions";
+import { Button } from "@/components/ui/button";
+import { useAuth } from "@/context/auth";
+import { Trash2Icon } from "lucide-react";
+import { useRouter } from "next/navigation";
+
+export default function RemoveFavouriteButton({
+  propertyId,
+}: {
+  propertyId: string;
+}) {
+  const auth = useAuth();
+  const router = useRouter();
+
+  return (
+    <Button
+      variant="outline"
+      onClick={async () => {
+        const tokenResult = await auth?.currentUser?.getIdTokenResult();
+        if (!tokenResult) {
+          return;
+        }
+        await removeFavourite(propertyId, tokenResult.token);
+        router.refresh();
+      }}
+    >
+      <Trash2Icon />
+    </Button>
+  );
+}
