bc0b9020a9f7e08764975c1d68e1938ccc634c05
Peter Ahumuza <hmz2peter@gmail.com>
2025-03-21 23:39:50 +0100

    delete account

---
+++ app/account/DeleteAccountButton.tsx
@@ -0,0 +1,97 @@
+"use client";
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { removeToken } from "@/context/actions";
+import { useAuth } from "@/context/auth";
+import {
+  deleteUser,
+  EmailAuthProvider,
+  reauthenticateWithCredential,
+} from "firebase/auth";
+import { useState } from "react";
+import { toast } from "sonner";
+
+export default function DeleteAccountButton() {
+  const auth = useAuth();
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [password, setPassword] = useState("");
+
+  const handleDeleteAccount = async () => {
+    if (auth?.currentUser?.email) {
+      setIsDeleting(true);
+      try {
+        await reauthenticateWithCredential(
+          auth.currentUser,
+          EmailAuthProvider.credential(auth.currentUser.email, password)
+        );
+        await deleteUser(auth.currentUser);
+        await removeToken();
+        toast("Account deleted successfully...!");
+      } catch (error) {
+        if (error instanceof Error && "code" in error) {
+          const title =
+            error.code === "auth/invalid-credential"
+              ? "Your password is incorrect"
+              : "An error occured";
+          toast(title);
+        } else {
+          toast("An unexpected error occurred");
+        }
+      }
+      setIsDeleting(false);
+    }
+  };
+
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button variant="destructive" className="w-full">
+          Delete Account
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>
+            Are you sure you want to delete you account
+          </AlertDialogTitle>
+          <AlertDialogDescription asChild>
+            <div>
+              This will permanetly delete your account and remove all your data
+              from our servers
+              <div>
+                <Label>Enter current password to continue</Label>
+                <Input
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  type="password"
+                />
+              </div>
+            </div>
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction
+            disabled={isDeleting}
+            onClick={handleDeleteAccount}
+          >
+            {isDeleting ? "Delete..." : "Delete Account"}
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+}
--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,10 +1,11 @@
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Label } from "@/components/ui/label";
 import { auth } from "@/firebase/server";
 import type { DecodedIdToken } from "firebase-admin/auth";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 import AccountUpdateForm from "./AccountUpdateForm";
+import DeleteAccountButton from "./DeleteAccountButton";
 
 export default async function Account() {
   const cookieStore = await cookies();
@@ -40,6 +41,12 @@ export default async function Account() {
 
           {isPasswordProvider && <AccountUpdateForm />}
         </CardContent>
+        {!decodedToken.admin && 
+        <CardFooter className="flex flex-col items-start">
+          <h2 className="text-red-500 font-bold text-2xl mb-2">Danger Zone</h2>
+          <DeleteAccountButton />
+        </CardFooter>
+        }
       </Card>
     </div>
   );
